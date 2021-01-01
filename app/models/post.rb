34bf995c6bbfd97cce74d89c2a56f780e021413c
juan_carlos_meza <juancmezat@gmail.com>
2020-12-31 22:56:51 -0800

    custom validation did not work for some reason

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%{value} is not a valid category" }
+    # validates :title_is_clickbaity
+
+    # def title_is_clickbaity
+    #     clickbaits = ["Won't Believe", "Secret", "Top ", "Guess"]
+    #     count = 0
+    #     clickbaits.each do |c|
+    #         if title.include?(c)
+    #             count += 1
+    #         end
+    #     end
+    #     if count == 0
+    #         errors.add(:title, "title is not clickbaity")
+    #     end
+    # end
 end
